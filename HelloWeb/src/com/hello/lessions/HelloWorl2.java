897dc28c1d788498f56f4fade730e260d1525f8e
Ad <AdI@Ad-pc>
2014-05-08 17:43:35 -0500

    my first check in

---
+++ HelloWeb/src/com/hello/lessions/HelloWorl2.java
@@ -0,0 +1,53 @@
+package com.hello.lessions;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorl2
+ */
+@WebServlet("/HelloWorl2")
+public class HelloWorl2 extends HttpServlet {
+	private static final long serialVersionUID = 2L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorl2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		{
+	        response.setContentType("text/html");
+	        PrintWriter out = response.getWriter();
+	        out.println("<html>");
+	        out.println("<head>");
+	        out.println("<title>Hello World! two</title>");
+	        out.println("</head>");
+	        out.println("<body>");
+	        out.println("<h1>Hello World! two</h1>");
+	        out.println("</body>");
+	        out.println("</html>");
+	    }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
